d66719840b1a6d0c5c7d0f0c7758a568ae79ad24
madhan-google <madhang9667@gmail.com>
2022-06-08 16:09:43 +0530

    first uploaded

---
+++ src/MainPackage/Solution.java
@@ -0,0 +1,8 @@
+package MainPackage;
+import java.util.*;
+public class Solution {
+	public static final Scanner sn = new Scanner(System.in);
+	public static void main(String[] asdf) {
+		System.out.println("hello");
+	}
+}
